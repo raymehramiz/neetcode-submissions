6887d8aba45bd5c47e563d015d0aa278a24cdfe0
Mahbod Rayan Mehramiz <48846247+raymehramiz@users.noreply.github.com>
2026-04-23 21:12:03 -0400

    Add: rotting-fruit - submission-0

---
+++ Data Structures & Algorithms/rotting-fruit/submission-0.go
@@ -0,0 +1,48 @@
+func orangesRotting(grid [][]int) int {
+    
+	rows, cols := len(grid), len(grid[0])
+	fresh := 0
+	time := 0
+
+	for r := 0; r < rows; r++{
+		for c := 0; c < cols; c++{
+			if grid[r][c] == 1{
+				fresh++
+			}
+		}
+	}
+
+	dirs := [][]int{{0,1}, {0,-1}, {1,0}, {-1,0}}
+
+	for fresh > 0 {
+		flipped := false
+		for r := 0; r < rows; r++{
+			for c:= 0; c < cols; c++{
+				if grid[r][c] == 2{
+					for _, dir := range dirs{
+						row, col := r +dir[0], c+dir[1]
+						if row >= 0 && row < rows && col >= 0 && col < cols&& grid[row][col] ==1{
+							grid[row][col] = 3
+							fresh--
+							flipped = true
+						}
+					}
+				}
+			}
+		}
+
+		if !flipped{
+			return -1
+		}
+
+		for r := 0; r < rows; r++{
+			for c := 0; c < cols; c++{
+				if grid[r][c] == 3{
+					grid[r][c] =2
+				}
+			}
+		}
+		time++
+	}
+	return time
+}
